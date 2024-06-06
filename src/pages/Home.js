58467639403a6e3c61f4db617967fe48941958bf
Atharv-28 <atharvmt@outlook.com>
2024-06-07 00:26:13 +0530

    Search Bar added to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import SearchBar from "../components/SearchBar";
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 const Home = () => {
-    return(
-        <div>
-            <h1>Here is Data</h1>
-        </div>
-    )
-}
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Users
+      </Typography>
+      <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+    </Container>
+  );
+};
 
 export default Home;
